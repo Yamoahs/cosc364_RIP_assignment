72b13e584646742634f9442371e1d57533c89000
Sam Yamoah <sya57@uclive.ac.nz>
2017-03-27 19:55:24 +1300

    Started layout of the config parser

---
+++ parser.py
@@ -0,0 +1,26 @@
+################################################################
+# File: parser.py                                              #
+# Author: sy, jl
+# Date created: 27/03/17
+# Date Modified: 27/03/17
+
+import sys
+import os.path
+
+################################################################
+## Global variables
+################################################################
+
+#IP address
+HOST =  "127.0.0.1"
+
+#valid Port no.
+VALID_PORTS =  range(1024, 64001)
+
+
+#### /END OF GLOBAL VARIABLES
+
+
+def config_parser():
+    """Main Parser function that will check config files are correct """
+    pass
