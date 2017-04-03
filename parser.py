7125abee400535ee8f9692f1b62afd904a46be9a
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-03 22:35:44 +1200

    added error checking for reading config files

--- parser.py
+++ parser.py
@@ -1,11 +1,12 @@
 ################################################################################
-# File: parser.py                                              
+# File: parser.py
 # Author: sy, jl
 # Date created: 27/03/17
 # Date Modified: 3/04/17
 ################################################################################
 import sys
 import os.path
+import re
 
 ################################################################################
 ## Global variables
@@ -23,65 +24,39 @@ args = (sys.argv)
 #Flag to make sure stdin arguemtents
 stdin_successful = False
 
+#Valid File Format
+PATTERN = re.compile("router[0-9]+_config")
 
 #### /END OF GLOBAL VARIABLES
 
-if len(args) == 5:
-    if len(args) != len(set(args)):
-        print("Duplicate Port Numbers")
+
+################################################################################
+## First check to make sure a valid config file is supplied
+################################################################################
+
+if len(args) == 2:
+    filename = args[1]
+    path = "./config_files/" + filename
+    if not PATTERN.match(filename):
+        print("FILE ERROR: File not a valid Configuration format")
         quit()
-    for port in args[1:-1]:
-        if int(port) not in VALID_PORTS:
-            print("port {} not a valid port".format(port))
-            quit()
-    sender_in_port = int(args[1])
-    sender_out_port = int(args[2])
-    chan_send_in_port = int(args[3])
-    filename = str(args[4])
-
-    #Checking if input file exists
-    if os.path.isfile(filename):
-        #Open File in byte mode
-        input_file = open(filename, 'rb')
-        stdin_successful = True
     else:
-        print('File does not exists')
-
+        #Check if config file exists
+        if os.path.isfile(path):
+            print("whoop whoop")
+            #Open File in normal mode
+            config_file = open(path, 'r')
+            stdin_successful = True
+        else:
+            print("FILE ERROR: File does not exist")
+            quit()
 else:
-    print("INPUT ERROR. Ports or File invalid")
+     print("INPUT ERROR: No Router Config file specified")
+     quit()
 
-#if stdin inputs are corerct begin socket initialisation
-if stdin_successful:
+#closing the file
+config_file.close()
 
-    print("IN PORT: {}\nOUT PORT: {}\CHAN SENDER IN PORT: {}\nFILENAME: {}"\
-    .format(sender_in_port, sender_out_port, chan_send_in_port, filename))
-
-
-    #Create the sockets
-    try:
-        sender_in_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except socket.error as e:
-        print(str(e))
-    try:
-        sender_out_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except socket.error as e:
-        print(str(e))
-
-    #Bind the sockets
-    try:
-        sender_in_socket.bind((HOST,sender_in_port))
-    except socket.error as e:
-        print(str(e))
-    try:
-        sender_out_socket.bind((HOST,sender_out_port))
-    except socket.error as e:
-        print(str(e))
-
-    #Connect the sockets
-    try:
-        sender_out_socket.connect((HOST,chan_send_in_port))
-    except socket.error as e:
-        print(str(e))
 
 
 def config_parser():
