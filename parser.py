be3ed83dc1cfb4913cfc3bb434156a5cd49ff83f
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-15 23:36:54 +1200

    Add a function to parse customer timers for routers

--- parser.py
+++ parser.py
@@ -22,13 +22,27 @@ VALID_PORTS =  range(1024, 64001)
 args = (sys.argv)
 
 #Flag for succesful load of config file
-config_successful = False
+load_successful = False
+
+#Flag for valid config file
+config_successful = True
+
+#Flag for valid customer timer parameter
+custom_timer = False
+valid_timer = False
 
 #Valid File Format
 CONFIG_PATTERN = re.compile("router[0-9]+_config")
 
 #### /END OF GLOBAL VARIABLES
 
+def print_config(router_id, input_ports, output_ports, timer = None):
+    print("Router ID: ", router_id)
+    print("Input Ports: ", input_ports)
+    print("Output Ports: ", output_ports)
+    if valid_timer:
+        print("timer: ", timer)
+##END OF FUNCTION
 
 ################################################################################
 ## First check to make sure a valid config file is supplied
@@ -46,7 +60,7 @@ if len(args) == 2:
             print("whoop whoop")
             #Open File in normal mode
             config_file = open(path, 'r')
-            config_successful = True
+            load_successful = True
         else:
             print("FILE ERROR: File does not exist")
             quit()
@@ -58,38 +72,71 @@ else:
 
 #TODO Do we want to check each file to make sure it has the 3 fields? (id, in/out)
 ##FUNCTION
-if config_successful:
-    info = []
+if load_successful:
+    config_info = []
     for line in config_file:
         #Filters out blank lines and comments. Comments start with "#"
         if not re.search(r'^\s|^#.*', line):
-            info.append(line.split())
-        # else:
-        #     print("comment found")
-    # print("info", info)
+            config_info.append(line.split())
+    # print(config_info)
 
-##collect the router ID
-router_id = info[0][1]
+    ##collect the router ID
+    if config_info[0][0] != "router-id" and len(config_info[0]) != 2:
+        config_successful = False
+        print("ROUTER-ID ERROR")
+        quit()
 
-##loop through the input Ports
-input_ports = []
-for port_numbr in info[1][1:]:
-    if port_numbr[-1] == ",":
-        port_numbr = port_numbr[:-1]
-    input_ports.append(int(port_numbr))
+    if config_info[1][0] != "input-ports" and len(config_info[1]) < 2:
+        config_successful = False
+        print("INPUT-PORT(S) ERROR")
+        quit()
 
-
-##loop through the output info
-output_ports = []
-for port_numbr in info[2][1:]:
-    output_ports.append(port_numbr.split("-"))
-    # print(port_numbr)
+    if config_info[2][0] != "output-ports" and len(config_info[2]) < 2:
+        config_successful = False
+        print("INPUT-PORT(S) ERROR")
+        quit()
 
 
-print("Router ID: ", router_id)
-print("Input Ports: ", input_ports)
-print("Output Ports: ", output_ports)
-##END OF FUNCTION
+    ##Check if timer values have been specified in the config file
+    if len(config_info) == 4:
+        custom_timer = True
+        if config_info[3][0] != "timer" and len(config_info[3]) != 2:
+            config_successful = False
+            print("TIMER ERROR")
+            quit()
+        else:
+            valid_timer = True
+
+##Set all the variables
+if config_successful:
+    #Router ID
+    router_id = config_info[0][1]
+
+    ##loop through the input Ports
+    input_ports = []
+    for port_numbr in config_info[1][1:]:
+        if port_numbr[-1] == ",":
+            port_numbr = port_numbr[:-1]
+        input_ports.append(int(port_numbr))
+
+    ##loop through the output config_info
+    output_ports = []
+    for port_numbr in config_info[2][1:]:
+        if port_numbr[-1] == ",":
+            port_numbr = port_numbr[:-1]
+        output_ports.append(port_numbr.split("-"))
+    for output in range(len(output_ports)):
+        output_ports[output] = list(map(int, output_ports[output]))
+
+    #Set timer if timer parameters have been provided
+    if custom_timer and valid_timer:
+        timer = config_info[3][1]
+        print_config(router_id, input_ports, output_ports, timer)
+
+    else:
+        print_config(router_id, input_ports, output_ports)
+
+
 
 
 #closing the file
