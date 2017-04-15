d206adb818c19aa3c7e3403e2f18002391f36531
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-16 02:32:08 +1200

    Added a run script NOTE: currently not functional

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# python3 daemon.py router1_config&
+# gnome-terminal -e "python3 daemon.py router2_config"
+# gnome-terminal -e "python3 daemon.py router3_config"
+# gnome-terminal -e "python3 daemon.py router4_config"
+# gnome-terminal -e "python3 daemon.py router5_config"
+# gnome-terminal -e "python3 daemon.py router6_config"
+# gnome-terminal -e "python3 daemon.py router7_config"
+
+
+python3 daemon.py router1_config&
+terminator -e "python3 daemon.py router2_config"&
+terminator -e "python3 daemon.py router3_config"&
+terminator -e "python3 daemon.py router4_config"&
+terminator -e "python3 daemon.py router5_config"&
+terminator -e "python3 daemon.py router6_config"&
+terminator -e "python3 daemon.py router7_config"&
