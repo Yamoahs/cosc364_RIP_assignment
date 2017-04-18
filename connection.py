f0def18857c888044bbe06beaa4f058fef4353d9
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-18 19:01:25 +1200

    Created, Binded, Connected & Closed Sockets

--- connection.py
+++ connection.py
@@ -10,6 +10,16 @@ import socket
 import struct
 import sys
 
+###############################################################################
+## Global variables
+################################################################################
+#IP address
+HOST =  "127.0.0.1"
+
+#### /END OF GLOBAL VARIABLES
+################################################################################
+
+
 class Router_connection():
     """docstring for Router_connection."""
     def __init__(self, parameters):
@@ -19,20 +29,32 @@ class Router_connection():
 
     def in_sockets(self):
         for i, port in enumerate(self.input_ports):
-            #Create the sockets
-            try:
-                self.input_sockets['input {}'.format(i)]\
-                = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            except socket.error as e:
-                print(str(e))
+            for neighbour in self.output_ports:
+                #Create the sockets
+                try:
+                    self.input_sockets[i] =\
+                    socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        # TODO: Close Sockets before testing
+                    #Bind the sockets
+                    self.input_sockets[i].bind((HOST, port))
+
+                    #Connect the sockets
+                    self.input_sockets[i].connect((HOST, neighbour[0]))
+
+                except socket.error as e:
+                    print(str(e))
+
         return self.input_sockets
+
+    def close_sockets(self):
+        for socket in self.input_sockets:
+            self.input_sockets[socket].close()
 
 
     def display_router_config(self):
         print("Router ID: ", self.router_id)
         print("Input Ports: ", self.input_ports)
-        print("Output Ports & metrics: ",self.output_ports)
+        print("Output Ports & metrics: ", self.output_ports)
         print("Custom Timer: ",self.timer)
+        print("\n Input Sockets:\n", self.input_sockets)
         # in_sockets()
