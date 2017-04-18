dffa66e33a44d350b4f18dc346334149a2f5a173
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-18 19:50:16 +1200

    trying to get the router to send basic message to each other

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import os.path
 import re
 import parser
 import connection
+import socket
 
 # Get the arguments list
 args = (sys.argv)
@@ -12,5 +13,23 @@ param = parser.get_params(args)
 
 router = connection.Router_connection(param)
 
-# router.display_router_config()
-print(router.in_sockets())
+
+router.in_sockets()
+info = router.display_router_config()
+router_id, input_ports, output_ports, timer, input_sockets = info
+
+data = "hello Reciever from router {}".format(router_id)
+while True:
+    for neighbour in input_sockets:
+        print(neighbour)
+        indat = input_sockets[neighbour].recv(512)
+        print(indat.decode('utf-8'))
+        input_sockets[neighbour].send(data.encode('utf-8'))
+
+
+print(data)
+    # sender_out_socket.send(data.encode('utf-8'))
+    # data = sender_in_socket.recv(512)
+    # print("From Reciever: ", data.decode('utf-8')
+
+router.close_sockets()
