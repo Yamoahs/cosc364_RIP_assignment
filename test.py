9881aaaa084a0855e4109007f7c5ecdf19a236f3
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-20 18:47:59 +1200

    started restructuring code, not quite running

--- test.py
+++ test.py
@@ -15,16 +15,21 @@ router = connection.Router_connection(param)
 
 
 router.in_sockets()
+router.out_sockets()
 info = router.display_router_config()
-router_id, input_ports, output_ports, timer, input_sockets = info
+router_id, input_ports, output_ports, timer, input_sockets, output_sockets = info
 
 data = "hello Reciever from router {}".format(router_id)
+
+
 while True:
+    for i in output_sockets:
+        output_sockets[i].send(data.encode('utf-8'))
     for neighbour in input_sockets:
-        print(neighbour)
+        # print(neighbour)
         indat = input_sockets[neighbour].recv(512)
         print(indat.decode('utf-8'))
-        input_sockets[neighbour].send(data.encode('utf-8'))
+
 
 
 print(data)
