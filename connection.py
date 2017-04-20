c9bfb0b1fdf7566905916f70331618234400ea53
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-20 18:47:12 +1200

    started restructuring code, not quite running

--- connection.py
+++ connection.py
@@ -26,6 +26,8 @@ class Router_connection():
         self.router_id, self.input_ports, self.output_ports, self.timer \
         = parameters
         self.input_sockets = {}
+        self.output_sockets = {}
+        self.neigbour_dist = {}
 
     def in_sockets(self):
         for i, port in enumerate(self.input_ports):
@@ -38,25 +40,44 @@ class Router_connection():
                     #Bind the sockets
                     self.input_sockets[i].bind((HOST, port))
 
-                    #Connect the sockets
-                    self.input_sockets[i].connect((HOST, neighbour[0]))
+                    # #Connect the sockets
+                    # self.input_sockets[i].connect((HOST, neighbour[0]))
 
                 except socket.error as e:
                     print(str(e))
 
         return self.input_sockets
 
+    def out_sockets(self):
+        for output in self.output_ports:
+            port, metric, next_hop = output
+            print(port, metric, next_hop)
+            #Create the sockets
+            try:
+                self.output_sockets[next_hop] =\
+                socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+                #Connect the sockets
+                self.output_sockets[next_hop].connect((HOST, port))
+
+            except socket.error as e:
+                print(str(e))
+        return self.output_sockets
+
     def close_sockets(self):
         for socket in self.input_sockets:
             self.input_sockets[socket].close()
+        for socket in self.output_sockets:
+            self.output_sockets[socket].close()
 
 
     def display_router_config(self):
-        print("Router ID: ", self.router_id)
-        print("Input Ports: ", self.input_ports)
-        print("Output Ports & metrics: ", self.output_ports)
-        print("Custom Timer: ", self.timer)
-        print("\n Input Sockets:\n", self.input_sockets)
-        print("\n")
+        # print("Router ID: ", self.router_id)
+        # print("Input Ports: ", self.input_ports)
+        # print("Output Ports & metrics: ", self.output_ports)
+        # print("Custom Timer: ", self.timer)
+        # print("\nInput Sockets:\n", self.input_sockets)
+        # print("\nOutput Sockets:\n", self.output_sockets)
+        # print("\n")
         return self.router_id, self.input_ports, self.output_ports, self.timer,\
-        self.input_sockets
+        self.input_sockets, self.output_sockets
