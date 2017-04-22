cd91ba033b92a0550331991acc58939522f25ef1
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-22 17:14:53 +1200

    restructured input and output sockets to init method

--- connection.py
+++ connection.py
@@ -29,26 +29,19 @@ class Router_connection():
         self.output_sockets = {}
         self.neigbour_dist = {}
 
-    def in_sockets(self):
+        #Create the input sockets
         for i, port in enumerate(self.input_ports):
-            for neighbour in self.output_ports:
-                #Create the sockets
-                try:
-                    self.input_sockets[i] =\
-                    socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            try:
+                self.input_sockets[i] =\
+                socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                #Bind the sockets
+                self.input_sockets[i].bind((HOST, port))
+                # #Connect the sockets
+                # self.input_sockets[i].connect((HOST, neighbour[0]))
+            except socket.error as e:
+                print(str(e))
 
-                    #Bind the sockets
-                    self.input_sockets[i].bind((HOST, port))
-
-                    # #Connect the sockets
-                    # self.input_sockets[i].connect((HOST, neighbour[0]))
-
-                except socket.error as e:
-                    print(str(e))
-
-        return self.input_sockets
-
-    def out_sockets(self):
+        #Create output Sockets and neighbour distances
         for output in self.output_ports:
             port, metric, next_hop = output
             print(port, metric, next_hop)
@@ -56,20 +49,58 @@ class Router_connection():
             try:
                 self.output_sockets[next_hop] =\
                 socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-                #Connect the sockets
-                self.output_sockets[next_hop].connect((HOST, port))
-
+                # #Connect the sockets
+                # self.output_sockets[next_hop].connect((HOST, port))
             except socket.error as e:
                 print(str(e))
-        return self.output_sockets
+
+            #neighbour distances
+            self.neigbour_dist[next_hop] = metric
+
+
+
+
+
+    # def in_sockets(self):
+    #     for i, port in enumerate(self.input_ports):
+    #         for neighbour in self.output_ports:
+    #             #Create the sockets
+    #             try:
+    #                 self.input_sockets[i] =\
+    #                 socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #
+    #                 #Bind the sockets
+    #                 self.input_sockets[i].bind((HOST, port))
+    #
+    #                 # #Connect the sockets
+    #                 # self.input_sockets[i].connect((HOST, neighbour[0]))
+    #
+    #             except socket.error as e:
+    #                 print(str(e))
+    #
+    #     return self.input_sockets
+
+    # def out_sockets(self):
+    #     for output in self.output_ports:
+    #         port, metric, next_hop = output
+    #         print(port, metric, next_hop)
+    #         #Create the sockets
+    #         try:
+    #             self.output_sockets[next_hop] =\
+    #             socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #
+    #             #Connect the sockets
+    #             self.output_sockets[next_hop].connect((HOST, port))
+    #
+    #         except socket.error as e:
+    #             print(str(e))
+    #     return self.output_sockets
 
     def close_sockets(self):
         for socket in self.input_sockets:
             self.input_sockets[socket].close()
         for socket in self.output_sockets:
             self.output_sockets[socket].close()
-
 
     def display_router_config(self):
         # print("Router ID: ", self.router_id)
@@ -80,4 +111,4 @@ class Router_connection():
         # print("\nOutput Sockets:\n", self.output_sockets)
         # print("\n")
         return self.router_id, self.input_ports, self.output_ports, self.timer,\
-        self.input_sockets, self.output_sockets
+        self.input_sockets, self.output_sockets, self.neigbour_dist
