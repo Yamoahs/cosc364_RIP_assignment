ea350b1c603f40a9010df9d95c794361896f0e84
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-22 19:19:56 +1200

    testing basic sending to all ports, routers are still waiting but no messages recv.

--- connection.py
+++ connection.py
@@ -25,17 +25,29 @@ class Router_connection():
     def __init__(self, parameters):
         self.router_id, self.input_ports, self.output_ports, self.timer \
         = parameters
-        self.input_sockets = {}
+        self.input_sockets = []
         self.output_sockets = {}
         self.neigbour_dist = {}
 
+        # #Create the input sockets
+        # for i, port in enumerate(self.input_ports):
+        #     try:
+        #         self.input_sockets[i] =\
+        #         socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #         #Bind the sockets
+        #         self.input_sockets[i].bind((HOST, port))
+        #         # #Connect the sockets
+        #         # self.input_sockets[i].connect((HOST, neighbour[0]))
+        #     except socket.error as e:
+        #         print(str(e))
+
         #Create the input sockets
-        for i, port in enumerate(self.input_ports):
+        for port in self.input_ports:
             try:
-                self.input_sockets[i] =\
-                socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 #Bind the sockets
-                self.input_sockets[i].bind((HOST, port))
+                s.bind((HOST, port))
+                self.input_sockets.append(s)
                 # #Connect the sockets
                 # self.input_sockets[i].connect((HOST, neighbour[0]))
             except socket.error as e:
@@ -44,7 +56,6 @@ class Router_connection():
         #Create output Sockets and neighbour distances
         for output in self.output_ports:
             port, metric, next_hop = output
-            print(port, metric, next_hop)
             #Create the sockets
             try:
                 self.output_sockets[next_hop] =\
@@ -58,47 +69,27 @@ class Router_connection():
             self.neigbour_dist[next_hop] = metric
 
 
+    def recieve_data(self):
+        available = select.select(self.input_sockets, [], [], 0.1)[0] # wait for 100ms, only interested in reading
+        serials = []
+        for socket in available:
+            serials.append(self.read_data(socket))
+        return serials
 
+    def read_data(self, in_sock):
+        data = in_socket.recv(4096) # only try to read at most 4096
+        return data.decode('utf-8') # decode it using utf-8
 
+    def send_table(self):
+        data = 'hello from router {}'.format(self.router_id)
+        for port, metric, next_hop in self.output_ports:
+            output = self.output_sockets[next_hop]
+            output.sendto(data.encode('utf-8'), (HOST, port))
 
-    # def in_sockets(self):
-    #     for i, port in enumerate(self.input_ports):
-    #         for neighbour in self.output_ports:
-    #             #Create the sockets
-    #             try:
-    #                 self.input_sockets[i] =\
-    #                 socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #
-    #                 #Bind the sockets
-    #                 self.input_sockets[i].bind((HOST, port))
-    #
-    #                 # #Connect the sockets
-    #                 # self.input_sockets[i].connect((HOST, neighbour[0]))
-    #
-    #             except socket.error as e:
-    #                 print(str(e))
-    #
-    #     return self.input_sockets
-
-    # def out_sockets(self):
-    #     for output in self.output_ports:
-    #         port, metric, next_hop = output
-    #         print(port, metric, next_hop)
-    #         #Create the sockets
-    #         try:
-    #             self.output_sockets[next_hop] =\
-    #             socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #
-    #             #Connect the sockets
-    #             self.output_sockets[next_hop].connect((HOST, port))
-    #
-    #         except socket.error as e:
-    #             print(str(e))
-    #     return self.output_sockets
 
     def close_sockets(self):
         for socket in self.input_sockets:
-            self.input_sockets[socket].close()
+            socket.close()
         for socket in self.output_sockets:
             self.output_sockets[socket].close()
 
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import re
 import parser
 import connection
 import socket
+from time import sleep, time
 
 # Get the arguments list
 args = (sys.argv)
@@ -14,25 +15,33 @@ param = parser.get_params(args)
 router = connection.Router_connection(param)
 
 
-router.in_sockets()
-router.out_sockets()
+# router.in_sockets()
+# router.out_sockets()
 info = router.display_router_config()
-router_id, input_ports, output_ports, timer, input_sockets, output_sockets = info
+router_id, input_ports, output_ports, timer, input_sockets, output_sockets, neigbour_dist = info
 
-data = "hello Reciever from router {}".format(router_id)
+print(output_ports)
+print(output_sockets)
 
-
-while True:
-    for i in output_sockets:
-        output_sockets[i].send(data.encode('utf-8'))
-    for neighbour in input_sockets:
-        # print(neighbour)
-        indat = input_sockets[neighbour].recv(512)
-        print(indat.decode('utf-8'))
+# data = "hello Reciever from router {}".format(router_id)
 
 
+# while True:
+#     for i in output_sockets:
+#         output_sockets[i].send(data.encode('utf-8'))
+#     for neighbour in input_sockets:
+#         # print(neighbour)
+#         indat = input_sockets[neighbour].recv(512)
+#         print(indat.decode('utf-8'))
 
-print(data)
+while(True):
+    sleep(0.05)
+    serials = router.recieve_data()
+    router.send_table()
+
+
+
+# print(data)
     # sender_out_socket.send(data.encode('utf-8'))
     # data = sender_in_socket.recv(512)
     # print("From Reciever: ", data.decode('utf-8')
