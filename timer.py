9187a60f7c792e1397c0ec930f3854f6bfac239c
Sam Yamoah <sya57@uclive.ac.nz>
2017-04-29 11:08:32 +1200

    started on the other timers

--- timer.py
+++ timer.py
@@ -8,12 +8,23 @@
 import time
 import sys
 
-def update_timer(timer):
-    t = timer
+def update_timer(u_timer):
+    t = u_timer
+    #TODO may have to randomise the offset
+    time.sleep(1) # Offset the update timer  
     while t != 0:
         time.sleep(1)
         t  -= 1
         print(t)
+
+def timeout(t_timer):
+    # Timeout time = 180sec
+    pass
+
+
+def garbage_collection_timer(g_timer):
+    #Garbage Timeout time =  120sec
+    pass
 
 
 update_timer(5)
