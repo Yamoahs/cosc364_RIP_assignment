a4d9aa770ccd7db53087752edbb922bd7a3d3745
sya57 <sya57@uclive.ac.nz>
2017-05-04 00:09:10 +1200

    Not sure what was changed (Linux Desktop)

--- timer.py
+++ timer.py
@@ -9,9 +9,10 @@ import time
 import sys
 
 def update_timer(u_timer):
+    print("update timer")
     t = u_timer
     #TODO may have to randomise the offset
-    time.sleep(1) # Offset the update timer  
+    time.sleep(1) # Offset the update timer
     while t != 0:
         time.sleep(1)
         t  -= 1
@@ -19,14 +20,24 @@ def update_timer(u_timer):
 
 def timeout(t_timer):
     # Timeout time = 180sec
-    pass
+    print("timeout timer")
+    t = t_timer
+    while t != 0:
+        time.sleep(1)
+        t  -= 1
+        print(t)
 
 
 def garbage_collection_timer(g_timer):
     #Garbage Timeout time =  120sec
-    pass
+    print("garbage collection timer")
+    t = g_timer
+    while t != 0:
+        time.sleep(1)
+        t  -= 1
+        print(t)
 
 
 update_timer(5)
-
-
+timeout(5)
+garbage_collection_timer(5)
