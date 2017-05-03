9b6c6bf00a276a3ca5d8e093923bb27bc98a4f22
sya57 <sya57@uclive.ac.nz>
2017-05-04 02:41:49 +1200

    Added functions to timer class. Needs to be tested

--- timer.py
+++ timer.py
@@ -44,37 +44,63 @@ garbage_collection_timer(5)
 
 class Timers(object):
 
-    def __init__(self):
+    def __init__(self, duration, tag, label):
         """ Set up some basics such as a collection of timers that you can use to
             store all the info. The details are for you to work out. I recommend
             you do not store duration, but end-time: it makes calculations easier.
         """
-        pass
+        self.time = time.time()
+        self.tag = tag
+        self.duration = duration
+        self.label = label
+        self.running = False
 
-    def add_timer(self, duration, id, message):
-        """ Add a timer of the given duration and (unique) ID.
-            It should also have some information stored with it.
-        """
-        pass
 
-    def remove_timer(self, id):
-        """ The timer with the given ID should be removed. That’s it really."""
-        pass
+    # def add_timer(self, duration, id, message):
+    #     """ Add a timer of the given duration and (unique) ID.
+    #         It should also have some information stored with it.
+    #     """
+    #     pass
+    #
+    # def remove_timer(self, id):
+    #     """ The timer with the given ID should be removed. That’s it really."""
+    #     pass
+    #
+    # def get_timeout(self):
+    #     """ Calculate the amount of time left until the next timer will go off.
+    #         If there are no timers left, that’s probably a bug.
+    #         Also, experience tells me that you don’t need to store anything
+    #         about which timer will expire, but you can if you wish.
+    #     """
+    #     pass
+    #
+    # def get_expired_timers(self):
+    #     """ A list of (id, message) tuples should be returned,
+    #         each of which belong to a timer that has expired since the last time
+    #         this function was called. It should also remove them from the collection.
+    #     """
+    #     pass
+
+    def start(self):
+        """Set the running flag to True"""
+        self.running = True
+
+    def stop(self):
+        """Set the running flag to False"""
+        self.running = False
+
+    def reset(self):
+        """Reset the clock ready for next use"""
+        self.time = time.time()
 
     def get_timeout(self):
         """ Calculate the amount of time left until the next timer will go off.
-            If there are no timers left, that’s probably a bug.
-            Also, experience tells me that you don’t need to store anything
-            about which timer will expire, but you can if you wish.
         """
-        pass
+        return ()(self.time + self.duration) - time.time())
+        
 
-    def get_expired_timers(self):
-        """ A list of (id, message) tuples should be returned,
-            each of which belong to a timer that has expired since the last time
-            this function was called. It should also remove them from the collection.
-        """
-        pass
+    def finished(self):
+        return self.running and time.time() >= (self.time + self.duration)
 
 
 if __name__ == '__main__':
